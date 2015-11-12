32173030198154256a560b72262727898853ad77
George Ornbo <george@shapeshed.com>
2015-11-12 08:15:13 +0000

    [feature] set git commit correctly

--- notes.sh
+++ notes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
  
-VERSION=0.0.2
+VERSION=0.0.3
 DATE=`date "+%Y-%m-%d"`
 NOTESFOLDER=~/notes
 export GIT_DIR=~/notes/.git
@@ -26,6 +26,8 @@ cmd_usage() {
     Delete a note
   $PROGRAM edit note-name
     Edit a note
+  $PROGRAM git
+    Run git commands
   $PROGRAM version
     Show version information.
 EOF
@@ -38,12 +40,20 @@ cmd_list() {
 cmd_new() {
   FILENAME=`echo $@ | tr ' ' '-' | tr '[A-Z]' '[a-z]' | tr -d '[{}(),\!]' | tr -d "\'"`
   ${EDITOR:-vi} $NOTESFOLDER/$DATE-$FILENAME.md
+  
+  local action="Add"
+	if [[ -f $NOTESFOLDER/$DATE-$FILENAME.md ]]; then
+		action="Edit"
+	fi
 	git add $DATE-$FILENAME.md
-	git commit -m "created note for $FILENAME"
+	git commit -m "$action note for $FILENAME"
 }
 
 cmd_edit() {
   ${EDITOR:-vi} $NOTESFOLDER/$@
+	git add $@
+  # BUG need a regex here to extract name
+	git commit -m "Edit note for $FILENAME"
 }
 
 cmd_delete() {
@@ -52,6 +62,10 @@ cmd_delete() {
 
 cmd_show() {
   more $NOTESFOLDER/$@
+}
+
+cmd_git() {
+  git "$@"
 }
 
 PROGRAM="${0##*/}"
@@ -65,6 +79,7 @@ case "$1" in
   show) shift; cmd_show "$@" ;;
   rm) shift; cmd_delete "$@" ;;
   version|--version|-v) shift; cmd_version "$@" ;;
+  git) shift; cmd_git "$@" ;;
   *) cmd_usage "$@" ;;
 esac
 exit 0
